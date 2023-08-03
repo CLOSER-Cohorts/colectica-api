c69a3c39067a2628f70ee45c62a8ec9a6f51d3c6
jli755 <jli755@gmail.com>
2023-08-02 23:42:26 -0700

    post release

--- colectica_api/__init__.py
+++ colectica_api/__init__.py
@@ -6,7 +6,7 @@ handles communication with a Colectica server (using its
 REST api).
 """
 
-__version__ = "0.0.4"
+__version__ = "0.0.5.dev0"
 
 from .colectica import ColecticaObject, ColecticaBasicAPI
 
