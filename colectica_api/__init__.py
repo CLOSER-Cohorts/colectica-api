4d7c953da10af4ec0c3e2d3cf4ba59d65c9e4e9d
jli755 <jli755@gmail.com>
2023-11-29 23:58:42 -0800

    Post release version bump

--- colectica_api/__init__.py
+++ colectica_api/__init__.py
@@ -6,7 +6,7 @@ handles communication with a Colectica server (using its
 REST api).
 """
 
-__version__ = "0.0.6"
+__version__ = "0.0.7.dev0"
 
 from .colectica import ColecticaObject, ColecticaBasicAPI
 
