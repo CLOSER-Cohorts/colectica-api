625303f5a2bd8dec2527c2a34d9edbf6c61f82b6
o.lyttleton@ucl.ac.uk <o.lyttleton@ucl.ac.uk>
2024-11-12 10:38:34 +0000

    Tidy up code, make it clearer. Remove the option to run the topic updates from the command line as a one-step process, ideally we shouldn't be allowing this as it makes it too easy to make incorrect updates to the data in the repository. Added a function that commits the updated topics to the repository.

--- examples/update_variables.py
+++ examples/update_variables.py
@@ -1,35 +1,29 @@
 """A set of functions that reassign variables to new topics.
 Use: python update_topics username password hostname(optional)
 
-Hostname will default to the a default host if it is not specified.
-
-The code can also detect if it is being called from the commandline as described
-above or if it is being imported into another Python file/REPL session. The variables
-for the username, password and hostname used to connect to a Colectica repository
-are declared by different means in either case. The usage for changing variable topics 
-from another Python file/REPL session is:
+The usage for changing variable topics  is:
 
 import examples.update_variables
-examples.update_variables.update_topics()
+updatedVariableGroups = examples.update_variables.update_topics()
+examples.update_variables.update_repository(updatedVariableGroups, 'Repository commit message')
 
 """
-import sys
 import pandas as pd
 import defusedxml
 from colectica_api import ColecticaObject
-# pip install openpyxl #needed for read-excel, doesn't have to be executed every time,
+from .lib.utility import get_namespace, find_reference, create_variable_reference
+# pip install openpyxl
+# needed for read-excel, doesn't have to be executed every time,
 # perhaps only the first time this script is run on a machine
 
-if __name__ != '__main__':
-    from .lib.utility import get_namespace, find_reference, create_variable_reference
-    USERNAME = "USERNAME"
-    PASSWORD = "PASSWORD"
-    HOSTNAME = "DEFAULT HOSTNAME"
-    C = ColecticaObject(HOSTNAME, USERNAME, PASSWORD,verify_ssl=False)
+USERNAME = "USERNAME"
+PASSWORD = "PASSWORD"
+HOSTNAME = "HOSTNAME"
+C = ColecticaObject(HOSTNAME, USERNAME, PASSWORD,verify_ssl=False)
 
 def get_current_state_of_item(agency_id, identifier, updated_groups, version=None):
-    """Checks to see if an item has already been read into memory. If it has, that
-    value stored in memory is returned. Otherwise a request is made to the REST API
+    """Checks to see if an item has already been read into memory. If it has, the
+    in-memory value is returned. Otherwise a request is made to the REST API
     for the most recent version of that item."""
     updated_referencing_item = [x for x in updated_groups if x[0] == identifier]
     if len(updated_referencing_item) > 0:
@@ -40,17 +34,17 @@ def get_current_state_of_item(agency_id, identifier, updated_groups, version=Non
         referencing_item = defusedxml.ElementTree.fromstring(fragment_xml)
     return referencing_item
 
-def update_list_of_referencing_groups(item_without_reference, agency, identifier, version, 
-                                      item_type, updated_refs_copy):
+def update_list_of_topic_groups(updated_group, agency, identifier, version, 
+                                      item_type, updated_groups_list):
     """Update the list of items read into memory. If the item being updated is already in the
     list, we update that item with the new version. If not, we append the item to the list."""
-    if ([x[0] for x in updated_refs_copy].count(identifier) > 0):
-        index_of_updated_ref = [x[0] for x in updated_refs_copy].index(identifier)
-        updated_refs_copy[index_of_updated_ref] = (
-            identifier, agency, version, item_type, item_without_reference)
+    if ([x[0] for x in updated_groups_list].count(identifier) > 0):
+        index_of_updated_ref = [x[0] for x in updated_groups_list].index(identifier)
+        updated_groups_list[index_of_updated_ref] = (
+            identifier, agency, version, item_type, updated_group)
     else:
-        updated_refs_copy.append(
-            (identifier, agency, version, item_type, item_without_reference))
+        updated_groups_list.append(
+            (identifier, agency, version, item_type, updated_group))
 
 # Declare an array which will contain instances of the variable groups that represent topics that
 # variables can be assigned to. We will be performing multiple updates to the groups, so instead of
@@ -62,16 +56,14 @@ def update_list_of_referencing_groups(item_without_reference, agency, identifier
 # Topics_to_be_changed-4-2024.xlsx, we will create a commit transaction using the Colectica REST
 # API, add all the variable groups in this array to that transaction, and commit the transaction.
 
-updatedReferencingGroups = []
+updated_topic_groups = []
 
 # Iterate through the rows in the spreadsheet. Each row contains details of a topic reassignment for
 # a variable...
 
-
 def update_topics():
-    """Main method for reassigning variables to new topics. The code reads a spreadsheet
-    containing details of new variable topic assignments, iterates through the spreadsheet 
-    and performs the reassignments. 
+    """Main method for reassigning variables to new topics. The code iterates through a spreadsheet
+    containing details of new variable topic assignments and performs the reassignments. 
     """
     data = pd.read_excel("Topics_to_be_changed-4-2024.xlsx")
     for topic_reassignment_details in [data.iloc[0,]]:
@@ -130,7 +122,7 @@ def update_topics():
                     # variable is currently assigned to.
                     source_item = get_current_state_of_item(source_group_identifiers['AgencyId'],
                                                        source_group_identifiers['Identifier'],
-                                                       updatedReferencingGroups,
+                                                       updated_topic_groups,
                                                        version=source_group_identifiers['Version']
                                                        )
                     # We get the current state of the variable group that we will be adding a
@@ -138,7 +130,7 @@ def update_topics():
                     # variable will be reassigned to.
                     destination_item = get_current_state_of_item(destination_group['AgencyId'],
                                                             destination_group['Identifier'],
-                                                            updatedReferencingGroups,
+                                                            updated_topic_groups,
                                                             version=destination_group['Version']
                                                             )
                     # Find and remove the reference to the variable in the variable group/topic.
@@ -180,33 +172,33 @@ def update_topics():
                         destination_item[0].append(new_reference)
                         # Finally we update the array containing the most current version of the
                         # variable group/topics.
-                        update_list_of_referencing_groups(source_item,
+                        update_list_of_topic_groups(source_item,
                            source_group_identifiers['AgencyId'],
                            source_group_identifiers['Identifier'],
                            source_group_identifiers['Version'],
                            C.item_code('Variable Group'),
-                           updatedReferencingGroups)
-                        update_list_of_referencing_groups(destination_item, 
+                           updated_topic_groups)
+                        update_list_of_topic_groups(destination_item, 
                            destination_group['AgencyId'],
                            destination_group['Identifier'],
                            destination_group['Version'],
                            C.item_code('Variable Group'),
-                           updatedReferencingGroups)
+                           updated_topic_groups)
                     else:
                         print(f"Variable {topic_reassignment_details.iloc[3]} in dataset "
                               f"{topic_reassignment_details.iloc[0]} is not assigned to a single "
                               "topic")
             else:
                 print(f"No dataset with alternate title {topic_reassignment_details[0]} found")
-    return updatedReferencingGroups
+    return updated_topic_groups
 
-if __name__ == '__main__':
-    from lib.utility import get_namespace, find_reference, create_variable_reference
-    USERNAME = sys.argv[1]
-    PASSWORD = sys.argv[2]
-    if len(sys.argv) == 4:
-        HOSTNAME = sys.argv[3]
-    else:
-        HOSTNAME = "DEFAULT HOSTNAME"
-    C = ColecticaObject(HOSTNAME, USERNAME, PASSWORD,verify_ssl=False)
-    update_topics()
+def update_repository(updated_items, transaction_message):
+    """Update the supplied items in the repository."""
+    transaction_response = C.create_transaction()
+    transaction_id = transaction_response['TransactionId']
+    for item in updated_items:
+        fragment_string = defusedxml.ElementTree.tostring(item[4], encoding='unicode')
+        C.add_items_to_transaction(item[1], item[0], item[2], fragment_string, 
+                                   C.item_code('Variable Group'), transaction_id)
+        commit_response = C.commit_transaction(transaction_id, transaction_message, 3)
+    return commit_response
