0fd51835557f3ad075844112717a7c2b7e79ba1a
o.lyttleton@ucl.ac.uk <o.lyttleton@ucl.ac.uk>
2024-11-19 09:10:55 +0000

    Address feedback from PR. Input file name is now provided as a function parameter.

--- examples/update_variables.py
+++ examples/update_variables.py
@@ -1,9 +1,10 @@
-"""A set of functions that reassign variables to new topics. This code assumes the existence of
-a file called 'Topics_to_be_changed.xlsx' in the same directory as this file. The set of 
-commands for changing variable topics is:
+"""A set of functions that reassign variables to new topics. The variable topic reassignments
+are defined in an Excel spreadsheet, the name of which is passed as an input argument to a
+function. An example of this spreadsheet ('Topics_to_be_changed.xlsx') is provided in the
+'examples' directory of this repository. The set of commands for changing variable topics is:
 
 import examples.update_variables
-updatedVariableGroups = examples.update_variables.update_topics()
+updatedVariableGroups = examples.update_variables.update_topics('Topics_to_be_changed.xlsx')
 examples.update_variables.update_repository(updatedVariableGroups, 'Repository commit message')
 
 You can verify that the above commands have successfully executed the topic reassignments by
@@ -78,23 +79,24 @@ variable_present_in_destination_topic = []
 
 # Name of spreadsheet file containing details of variables and topics
 
-INPUT_FILE_NAME = "Topics_to_be_changed.xlsx"
-
-def update_topics():
+def update_topics(input_file_name):
     """Method for reassigning variables to new topics. The code iterates through a spreadsheet
     containing details of new variable topic assignments and performs the reassignments. 
     """
-    data = pd.read_excel(INPUT_FILE_NAME)
+    print(f"Reading topic reassignments from {input_file_name}")
+    data = pd.read_excel(input_file_name)
     # Iterate through the rows in the spreadsheet. Each row contains details of a topic
     # reassignment for a variable...
     for topic_reassignment_details in data.iloc:
         print("Performing the following topic reassignment...")
-        print(topic_reassignment_details)
         # Search for the physical instance/dataset item which contains the variable in the current
         # input file row....
+        print(topic_reassignment_details)
         physical_instance_containing_variable = C.search_item(
-            'a51e85bb-6259-4488-8df2-f08cb43485f8', topic_reassignment_details.iloc[0].strip(), 0, 
-               SearchLatestVersion=True)['Results']
+            'a51e85bb-6259-4488-8df2-f08cb43485f8',
+            str(topic_reassignment_details.iloc[0]).strip(),
+            0,
+            SearchLatestVersion=True)['Results']
         if len(physical_instance_containing_variable) == 1:
             # We need to search within the physical instance/dataset for the variable named in the
             # current row. We create a JSON object representing the physical instance/dataset.
@@ -107,7 +109,7 @@ def update_topics():
             # variable we are reassigning to a new topic. The 'SearchSets' keyword argument
             # represents the physical instance/dataset we are searching for that variable in.
             variables_metadata = C.search_items(C.item_code('Variable'), SearchSets=search_sets,
-               SearchTerms=[topic_reassignment_details.iloc[1].strip()])['Results']
+               SearchTerms=[str(topic_reassignment_details.iloc[1]).strip()])['Results']
             if len(variables_metadata) == 1:
                 variable_agency_id = variables_metadata[0]['AgencyId']
                 variable_identifier = variables_metadata[0]['Identifier']
@@ -236,11 +238,11 @@ def update_topics():
                 print(f"A single instance of the variable {topic_reassignment_details.iloc[1]} "
                       f"has not been found. Either none or multiple instances were found.")
         else:
-            print(f"No dataset with alternate title {topic_reassignment_details[0]} found")
+            print(f"No dataset with alternate title {topic_reassignment_details.iloc[0]} found")
     number_of_successful_topic_reassignments = len([x for x in variable_not_present_in_source_topic
                                            if x in variable_present_in_destination_topic])
     print(f"{number_of_successful_topic_reassignments} of {len(data)} variables in the input file "
-          f"{INPUT_FILE_NAME} were successfully assigned to new topics.")
+          f"{input_file_name} were successfully assigned to new topics.")
     if (len(variable_not_present_in_source_topic) == len(data) and
        len(variable_present_in_destination_topic) == len(data)):
        print("The variable topic reassignments in the input data file have already all been "
