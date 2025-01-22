01635696d40575db0fd1b751e9b5135fd40ecba9
o.lyttleton@ucl.ac.uk <o.lyttleton@ucl.ac.uk>
2025-01-22 11:55:14 +0000

    add bracket in comment

--- examples/lib/utility.py
+++ examples/lib/utility.py
@@ -137,7 +137,7 @@ def get_item_from_topic_name(topic_name, topic_type, containing_item_name, conta
 def get_topic_for_item(agency_id, identifier, version, item_type):
     """This function gets the topic item(s) for an item (i.e. question/variable), given the
     question/variable's agency id, identifier, version, and the UUID code representing the item's
-    type (e.g. C.item_code("Variable Group").
+    type (e.g. C.item_code("Variable Group")).
     """
     topics_assigned_to_item=[]
     related_groups = C.search_relationship_byobject(agency_id, identifier, Version=version, item_types=[item_type])
