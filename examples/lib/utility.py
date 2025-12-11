32a074b0d6c597e87307a815d9bb30eac2e5544f
o.lyttleton@ucl.ac.uk <o.lyttleton@ucl.ac.uk>
2025-12-11 11:19:23 +0000

    wip

--- examples/lib/utility.py
+++ examples/lib/utility.py
@@ -266,14 +266,12 @@ def get_item_from_topic_name(topic_name, topic_type, containing_item, C, groupsI
                             version=varGroup['Item1']['Item2'])
                         level_zero_group=get_level_zero_group_for_topic(var_group_item, C)
                         if level_zero_group is not None:
-                            level_zero_groups.append(level_zero_group)
-                            print(level_zero_group[0])
-                            print(level_zero_group[0][2].text)            
+                            level_zero_groups.append(level_zero_group)            
                 if len(set([x[0][2].text for x in level_zero_groups]))==1:
                     containing_level_zero_group = [{
-                    "AgencyId": level_zero_group[0][1].text,
-                    "Identifier": level_zero_group[0][2].text,
-                    "Version": level_zero_group[0][3].text,
+                    "AgencyId": level_zero_groups[0][0][1].text,
+                    "Identifier": level_zero_groups[0][0][2].text,
+                    "Version": level_zero_groups[0][0][3].text,
                     }]
                     datasetToZeroGroupMappings[get_urn_from_item(containing_item)]=containing_level_zero_group
                 # Do a search for the first three numbers of the topic group, and then filter
